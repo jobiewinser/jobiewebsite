32e2170980bf4de9db33d484fbfe680052fb3da4
jobie winser <jobiewinser@live.co.uk>
2021-08-01 22:14:45 +0100

    Committing to test static files

--- settings.py
+++ settings.py
@@ -141,8 +141,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/jobiewebsite/static/',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
