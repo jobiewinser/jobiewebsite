d94028210e3633ab8191afe9b43a074a463b3673
jobie winser <jobie.winser@abingdon.org.uk>
2021-08-05 15:35:24 +0100

    Changed to an absolute path for env file as it's the same on prod and dev

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path  # python3 only
 
-env_path = Path('.env')
+env_path = Path('/var/www/jobiewebsite/.env')
 load_dotenv(dotenv_path=env_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
