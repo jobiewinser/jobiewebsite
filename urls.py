8cd1c95e1867df934b1d1a1bfebf1cb0519fd6da
jobie winser <jobie.winser@abingdon.org.uk>
2022-08-23 10:25:18 +0100

    added auth api

--- urls.py
+++ urls.py
@@ -21,7 +21,10 @@ from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from  rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(core.urls)),
+    path('api/auth/', obtain_auth_token),
+    path('api/', include('api.routers'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
